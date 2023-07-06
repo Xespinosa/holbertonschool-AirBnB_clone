c97a2019d2713d66ce4ab65a76e7eb7e605c2a4e
Crimson Cross <betancourtxavier77@gmail.com>
2023-07-06 01:30:04 -0400

    uploading user module

--- models/user.py
+++ models/user.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+"""Module containing the user
+"""
+
+
+class User(BaseModel):
+    """user class that inherets from base model
+
+    Args:
+        BaseModel (class): parent class
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
